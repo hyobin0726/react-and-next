dc0730f858f49ae98eb5bbae4a85df9868849ef9
hyobin0726 <hyobin0726@naver.com>
2024-02-28 00:25:54 +0900

    react install

--- section2/src/main.jsx
+++ section2/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// root아래 렌더링
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
