1ebbd1424a2394c58af164091142c0b4adfeaf3c
hyobin0726 <hyobin0726@naver.com>
2024-03-02 02:19:50 +0900

    3강 jsx

---
+++ section2/src/components/Header.jsx
@@ -0,0 +1,9 @@
+//함수컴포넌트
+function Header(){
+     return(
+       <header>
+         <h1>HEADER</h1>
+       </header>
+     );
+   }
+export default Header;
--- section2/src/main.jsx
+++ section2/src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 
 // root아래 렌더링
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
+
 )
