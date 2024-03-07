dfac49f3c8fdad91675512701df3f20c0bdae7ad
{hyobin0726} <{hyobin0726@naver.com}>
2024-03-07 11:58:08 +0900

    state로 사용자 입력관리, ref

--- section2/src/components/Body.jsx
+++ section2/src/components/Body.jsx
@@ -1,41 +1,61 @@
 import './Body.css';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
-function Lightbulb({ light }){
-     return(
-          <>
-          {light == "ON" ? (
-               <div style={{ backgroundColor : "orange"}}>ON</div>
-          ):(
-               <div style={{ backgroundColor : "gray"}}>OFF</div>
-          )}
-          </>
-     );
-}
-
-function StaticLightbulb (){
-     return <div style={{backgroundColor : "gray"}}>OFF</div>; 
-}
 export default function Body(){
-     const [light, setLight] = useState("OFF");
+     const [state, setState] = useState({
+          name : "",
+          gender : "",
+          bio : ""
 
+     });
+
+     const onChange=(e)=> {
+          console.log(e.target.name + " : " + e.target.value);
+          setState({
+               ...state,
+               [e.target.name] : e.target.value,
+          });
+     };
+     const nameRef = useRef();
+     const onSubmit = () =>{
+          if (state.name === "") {
+               nameRef.current.focus();
+               return
+          }
+          console.log(state);
+          alert(`${state.name}님 회원가입을 축하합니다!`);
+     }
      return (
      <div className="body">
-          <Lightbulb light={light}  />     
-          <StaticLightbulb />
-          <button onClick={() => {
-          setLight("ON");
-     }}
-     >
-          켜기
-     </button>
-     <button 
-          onClick={()=>{
-               setLight("OFF");
-          }}
-     >
-          끄기
-          </button>
+          <div>
+          <input
+               ref={nameRef}
+               name={"name"}
+               value={state.name}
+               onChange={onChange} />
+          </div>    
+          <div>
+               <select
+               name={"gender"}
+               value={state.gender}
+               onChange={onChange} 
+               >
+                    <option value="">밝히지 않음</option>
+                    <option value="female">여성</option>
+                    <option value="male">남성</option>
+               </select>
+               </div>
+          <div>
+               <textarea
+               name={"bio"}
+               value={state.bio}
+               onChange={onChange}  />
+               
+          </div>
+          <div>
+               <button onClick={onSubmit}>회원가입</button>
+          </div>
      </div>
+     
      );
      }
